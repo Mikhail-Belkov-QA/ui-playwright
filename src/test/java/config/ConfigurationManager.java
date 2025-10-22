e1b4680ba7b4eb8d9f13835432d710ebcfb66007
MikhailBelkov <mgarold@yandex.ru>
2025-10-22 11:52:00 +0300

    Доработка

--- src/test/java/config/ConfigurationManager.java
+++ src/test/java/config/ConfigurationManager.java
@@ -2,8 +2,26 @@ package config;
 
 import org.aeonbits.owner.ConfigCache;
 
-public class ConfigurationManager {
+public final class ConfigurationManager {
+    private static volatile Configuration instance;
+
+    private ConfigurationManager() {}
+
+    public static Configuration getInstance() {
+        Configuration local = instance;
+        if (local == null) {
+            synchronized (ConfigurationManager.class) {
+                local = instance;
+                if (local == null) {
+                    local = ConfigCache.getOrCreate(Configuration.class);
+                    instance = local;
+                }
+            }
+        }
+        return local;
+    }
+
     public static Configuration config() {
-        return ConfigCache.getOrCreate(Configuration.class);
+        return getInstance();
     }
 }
