68cf8fde95883f2b0440f8959913d23819a6e705
MikhailBelkov <mgarold@yandex.ru>
2025-10-22 09:55:49 +0300

    Доработка

--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -37,8 +37,8 @@ public class ProductsTest extends BaseTest {
         assertThat(cartPage.getItems()).containsText(firstItemName);
 
         ShipInfo shipInfo = ShipInfo.builder()
-                .firstName("Oleg")
-                .lastName("ThreadQA")
+                .firstName("Misha")
+                .lastName("Belkov")
                 .zip("123456")
                 .build();
 
